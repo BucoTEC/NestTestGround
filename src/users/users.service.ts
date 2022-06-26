b5281dcdf47ea64da69b94e2aa898df3a29b42c1
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-26 17:13:38 +0200

    remote setup test

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   helloFromUsers() {
-    return 'hello from users';
+    return 'hello from users global service ';
   }
 }
